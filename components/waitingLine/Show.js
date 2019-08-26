7bd02d3c4b4355358507bd452ffb3a9956828db0
Grégoire Hébert <gregoire@les-tilleuls.coop>
2019-08-26 22:03:59 +0200

    Add carousel at start, not sexy but hey it could be (https://github.com/archriss/react-native-snap-carousel)

--- components/waitingLine/Show.js
+++ components/waitingLine/Show.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text, Dimensions } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 import Spinner from '../Spinner';
 import { init } from '../../actions/waitingLine/show';
+
 import QRCode from 'react-native-qrcode-svg';
 import CountDown from 'react-native-countdown-component';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
 class Show extends Component {
 
   componentDidMount() {
     this.props.init();
+  }
+
+  _renderItem ({item, index}) {
+    return (
+        <View style={{ height: viewportHeight }}>
+            <Text style={styles.title}>{ item.title }</Text>
+        </View>
+    );
   }
 
   render() {
@@ -46,26 +58,44 @@ class Show extends Component {
           <Button title="cancel" raised={true}/>
         </Card>
       </View>
-  );
+    );
 
     if (item && item.started && !item.waiting && !item.ready) return <Card><QRCode value="{item['customerId']}" size={300} /></Card>;
 
+    if (item && !item.started && !item.waiting && !item.ready) return (
+      <Carousel
+        data={[
+            {title: "It prevent you from waiting in a queue, and will warn you at the right time. Waiting for a table at a restaurant... It's simple and works in a few steps."},
+            {title: "Present your QrCode."},
+            {title: "Wait for your turn. The application detects that the QrCode has been flashed and redirect you in the waiting room. You can do anything you want until then (shopping, grab a couple of beers, run a marathon, etc) :)"},
+            {title: "Changed your mind? Nothing to worry, you can always cancel and leave the waiting room."},
+            {title: "It's your turn? The application will receive a signal, so as you ;)"},
+        ]}
+        renderItem={this._renderItem}
+        sliderWidth={viewportWidth}
+        itemWidth={viewportWidth}
+        slideStyle={{ width: viewportWidth }}
+        inactiveSlideOpacity={1}
+        inactiveSlideScale={1}
+      />
+    );
+
     return (
-        <View style={ {flex: 1} }>
-          <ScrollView>
-            {item &&
-                <React.Fragment>
-                  <Card>
-                      <Text>{item['customerId']}</Text>
-                      <Text>{item['ready'] ? 'ready' : 'not Ready'}</Text>
-                      <Text>{item['started'] ? 'started' : 'not started'}</Text>
-                      <Text>{item['waiting'] ? 'waiting' : 'not waiting'}</Text>
-                  </Card>
-                </React.Fragment>
-            }
-            {this.props.error && <View style={viewStyle}><Text style={textStyleAlert}>{this.props.error}</Text></View>}
-          </ScrollView>
-        </View>
+      <View style={ {flex: 1} }>
+        <ScrollView>
+          {item &&
+            <React.Fragment>
+              <Card>
+                <Text>{item['customerId']}</Text>
+                <Text>{item['ready'] ? 'ready' : 'not Ready'}</Text>
+                <Text>{item['started'] ? 'started' : 'not started'}</Text>
+                <Text>{item['waiting'] ? 'waiting' : 'not waiting'}</Text>
+              </Card>
+            </React.Fragment>
+          }
+          {this.props.error && <View style={viewStyle}><Text style={textStyleAlert}>{this.props.error}</Text></View>}
+        </ScrollView>
+      </View>
     );
   }
 }
